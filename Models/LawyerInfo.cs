28a0fa61c6b0ab2727aaf92bce120ccd3bba1e95
doaaadel1999 <doaaadel252@yahoo.com>
2022-11-06 19:23:10 +0200

    Created project repo

---
+++ Models/LawyerInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Encyclopedia_Of_Laws.Models
+{
+    public class LawyerInfo
+    {
+        [Key]
+        public int LawyerId { get; set; }
+        public string Specialization { get; set; }
+        public string OfficeNumber { get; set; }
+        public string OfficeLocation { get; set; }
+        public string Information { get; set; }
+        public string JopDescription { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
